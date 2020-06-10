038b971ef8ea65567cbb7311f4c83c90cb717274
ajcardenasb <ajcardenasb1@gmail.com>
2020-06-10 11:24:36 +0200

    adding RNAseq pipeline

---
+++ coral_deoxygenation_RNASeq.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#########################################
+#########  Trimming adaptors  ##########
+#########################################
+for i in *_R1_001.fastq.gz ; do java -jar /home/cardena/software/Trimmomatic-0.38/trimmomatic-0.38.jar PE -threads 16 $i ${i//_R1_/_R2_} -baseout ${i//_R1_001.fastq.gz/.fastq} ILLUMINACLIP:/home/cardena/software/Trimmomatic-0.38/adapters/Truseq_V.3_edited.fa:2:30:10:8:TRUE LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:50; done
+
+########################################################
+#########  Mapping To a. millepora gene set   ##########
+########################################################
+bowtie2-build Amil.all.maker.transcripts.fasta  Amil_DB ## available from https://przeworskilab.com/data/
+for i in *_1P.fastq ; do bowtie2 -p 14 --very-sensitive-local -x Amil_DB -1 $i -2 ${i//1P.fastq/2P.fastq} -S $(basename $i | sed  's/_1P.fastq/.sam/') ; done
+
+############################################
+#########  Quantify transcripts   ##########
+############################################
+for i in *R*0-*.sam ; do /home/cardena/software/express-1.5.1-linux_x86_64/express -o $(basename $i | sed 's/M_19_[0][34][0-9][0-9]_//' | sed 's/_D[0-9][0-9][0-9]-D[0-9][0-9][0-9]_L00[0-8]//' | sed 's/.\///'| sed 's/.sam/_express/') Trinity_A.tenuis.fasta $i ; done
